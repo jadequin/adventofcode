68e8e79c3c86577c4b30d58e52ea88300826589a
Till Nitsche <till.nitsche@web.de>
2020-11-24 23:11:59 +0100

    d5 hashSequence

--- aoc16/d5/p1.kts
+++ aoc16/d5/p1.kts
@@ -1,3 +1,17 @@
 import java.io.File
+import java.math.BigInteger
+import java.nio.charset.Charset
+import java.security.MessageDigest
 
 val input = File("input.txt")
+
+val doorId = input.readText()
+
+val doorIdBytes = doorId.toByteArray(Charset.defaultCharset())
+val digest = MessageDigest.getInstance("MD5").digest(doorIdBytes)
+val bigInt = BigInteger(1, digest)
+val hashtext = "0".repeat(32 - bigInt.toString(16).length) + bigInt.toString(16);
+
+val hashSequence = generateSequence(0, Int::inc).map {  }
+
+println(hashtext)
